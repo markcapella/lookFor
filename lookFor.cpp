01c8939a35f1d07f38d411bb220b848949714931
Mark Capella <markcapella@twcny.rr.com>
2023-09-17 16:52:46 -0400

    cleanup lfl filesize column

--- lookFor.cpp
+++ lookFor.cpp
@@ -1,25 +1,21 @@
+#include <dirent.h>
 #include <filesystem>
 #include <iostream>
-#include <dirent.h>
 #include <string>
 #include <unistd.h>
 
-
 using namespace std;  
-using STRING = std::string;
-
-namespace FS = std::filesystem;
-
+#define FILE_SYS std::filesystem
 
 // Local forward decls
-STRING findParm(STRING, int, char* []);
+string findParm(string, int, char* []);
 
-void listFilesInDir(const STRING&, const STRING&, const STRING&);
+void listFilesInDir(const string&, const string&, const string&);
 
-void filterAndPrint(const STRING&, const STRING&, const STRING&);
+void filterAndPrint(const string&, const string&, const string&);
 
-static bool startsWith(const STRING&, const STRING&);
-static bool endsWith(const STRING&, const STRING&);
+static bool startsWith(const string&, const string&);
+static bool endsWith(const string&, const string&);
 
 
 // *****************************************************************
@@ -29,35 +25,35 @@ static bool endsWith(const STRING&, const STRING&);
 int main(int argc, char* argv[]) {
 
     // String, and guard the inputs.
-    STRING fileNameString = "";
-    STRING targetPathString = ".";
-    STRING endsWithString = "";
+    string fileNameString = "";
+    string targetPathString = ".";
+    string endsWithString = "";
 
     if (argc > 1) {
-        fileNameString = std::string(argv[1]);
+        fileNameString = string(argv[1]);
         if (argc > 2) {
-            targetPathString = std::string(argv[2]);
+            targetPathString = string(argv[2]);
             if (argc > 3) {
-                endsWithString = std::string(argv[3]);
+                endsWithString = string(argv[3]);
             }
         }
     }
 
     // Default to working directory.
-    FS::path targetPath;
+    FILE_SYS::path targetPath;
     if (targetPathString.empty()) {
-        targetPath = FS::canonical(FS::current_path());
+        targetPath = FILE_SYS::canonical(FILE_SYS::current_path());
     } else {
         // Ensure target exists, and isn't a directory.
-        if (!FS::exists(targetPathString)) {
+        if (!FILE_SYS::exists(targetPathString)) {
             cout << "Target doesn\'t exist, halting." << endl;
             return 0;
         }
-        if (!FS::is_directory(FS::status(targetPathString))) {
+        if (!FILE_SYS::is_directory(FILE_SYS::status(targetPathString))) {
             cout << "Target isn\'t a directory, halting." << endl;
             return 0;
         }
-        targetPath = FS::canonical(targetPathString);
+        targetPath = FILE_SYS::canonical(targetPathString);
     }
 
     // Gopher it!
@@ -68,21 +64,17 @@ int main(int argc, char* argv[]) {
 // *** Main loop, recurse through dirs, print matches            ***
 // *****************************************************************
 
-void listFilesInDir(const STRING& targetPath, const STRING& fileNameString,
-                    const STRING& endsWithString) {
+void listFilesInDir(const string& targetPath, const string& fileNameString,
+                    const string& endsWithString) {
 
     DIR* dirFileHandle = opendir(targetPath.c_str());
     if (!dirFileHandle) {
-        // cout << "listFilesInDir() targetPath: " << targetPath <<
-        //           " not read due to error: " << errno << endl;
         return;
     }
 
     struct dirent* dirFileEntry;
     while ((dirFileEntry = readdir(dirFileHandle)) != NULL) {
-        STRING newTargetPath = dirFileEntry->d_name;
-        // cout << "listFilesInDir() reads : " << newTargetPath << " size: " <<
-        //     newTargetPath.length() << endl;
+        string newTargetPath = dirFileEntry->d_name;
 
         // Ignore "movement" entries.
         if (endsWith(newTargetPath, "..") || endsWith(newTargetPath, ".")) {
@@ -99,7 +91,7 @@ void listFilesInDir(const STRING& targetPath, const STRING& fileNameString,
             continue;
         }
 
-        // Process first entry.
+        // Process entry.
         filterAndPrint(newTargetPath, fileNameString, endsWithString);
 
         // If dir/folder, drop in and process all, else we're just done.
@@ -115,12 +107,12 @@ void listFilesInDir(const STRING& targetPath, const STRING& fileNameString,
 // *** Helper method for each dir entry                          ***
 // *****************************************************************
 
-void filterAndPrint(const STRING& targetPath, const STRING& fileNameString,
-                    const STRING& endsWithString) {
+void filterAndPrint(const string& targetPath, const string& fileNameString,
+                    const string& endsWithString) {
 
     // Ensure fileNameString match.
     if (!fileNameString.empty()) {
-        if (targetPath.find(fileNameString) == STRING::npos) {
+        if (targetPath.find(fileNameString) == string::npos) {
             return;
         }
     }
@@ -141,12 +133,12 @@ void filterAndPrint(const STRING& targetPath, const STRING& fileNameString,
 // *****************************************************************
 
 static bool
-startsWith(const STRING& str, const STRING& prefix) {
+startsWith(const string& str, const string& prefix) {
     if (prefix.size() > str.size()) {
         return false;
     }
 
-    return std::equal(str.begin(), str.begin() + prefix.size(), prefix.begin());
+    return equal(str.begin(), str.begin() + prefix.size(), prefix.begin());
 }
 
 // *****************************************************************
@@ -154,10 +146,10 @@ startsWith(const STRING& str, const STRING& prefix) {
 // *****************************************************************
 
 static bool
-endsWith(const STRING& str, const STRING& postfix) {
+endsWith(const string& str, const string& postfix) {
     if (postfix.size() > str.size()) {
         return false;
     }
 
-    return std::equal(str.begin() + str.size() - postfix.size(), str.end(), postfix.begin());
+    return equal(str.begin() + str.size() - postfix.size(), str.end(), postfix.begin());
 }
--- lookForLong.cpp
+++ lookForLong.cpp
@@ -1,25 +1,22 @@
+#include <dirent.h>
 #include <filesystem>
 #include <iostream>
-#include <dirent.h>
-#include <string>
+#include <string.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
-
 using namespace std;
-using STRING = std::string;
-
-namespace FS = std::filesystem;
-
+#define FILE_SYS std::filesystem
 
 // Local forward decls
-STRING findParm(STRING, int, char* []);
+string findParm(string, int, char* []);
 
-void listFilesInDir(const STRING&, const STRING&, const STRING&);
+void listFilesInDir(const string&, const string&, const string&);
 
-void filterAndPrint(const STRING&, const STRING&, const STRING&);
+void filterAndPrint(const string&, const string&, const string&);
 
-static bool startsWith(const STRING&, const STRING&);
-static bool endsWith(const STRING&, const STRING&);
+static bool startsWith(const string&, const string&);
+static bool endsWith(const string&, const string&);
 
 
 // *****************************************************************
@@ -29,35 +26,35 @@ static bool endsWith(const STRING&, const STRING&);
 int main(int argc, char* argv[]) {
 
     // String, and guard the inputs.
-    STRING fileNameString = "";
-    STRING targetPathString = ".";
-    STRING endsWithString = "";
+    string fileNameString = "";
+    string targetPathString = ".";
+    string endsWithString = "";
 
     if (argc > 1) {
-        fileNameString = std::string(argv[1]);
+        fileNameString = string(argv[1]);
         if (argc > 2) {
-            targetPathString = std::string(argv[2]);
+            targetPathString = string(argv[2]);
             if (argc > 3) {
-                endsWithString = std::string(argv[3]);
+                endsWithString = string(argv[3]);
             }
         }
     }
 
     // Default to working directory.
-    FS::path targetPath;
+    FILE_SYS::path targetPath;
     if (targetPathString.empty()) {
-        targetPath = FS::canonical(FS::current_path());
+        targetPath = FILE_SYS::canonical(FILE_SYS::current_path());
     } else {
         // Ensure target exists, and isn't a directory.
-        if (!FS::exists(targetPathString)) {
+        if (!FILE_SYS::exists(targetPathString)) {
             cout << "Target doesn\'t exist, halting." << endl;
             return 0;
         }
-        if (!FS::is_directory(FS::status(targetPathString))) {
+        if (!FILE_SYS::is_directory(FILE_SYS::status(targetPathString))) {
             cout << "Target isn\'t a directory, halting." << endl;
             return 0;
         }
-        targetPath = FS::canonical(targetPathString);
+        targetPath = FILE_SYS::canonical(targetPathString);
     }
 
     // Gopher it!
@@ -68,21 +65,17 @@ int main(int argc, char* argv[]) {
 // *** Main loop, recurse through dirs, print matches            ***
 // *****************************************************************
 
-void listFilesInDir(const STRING& targetPath, const STRING& fileNameString,
-                    const STRING& endsWithString) {
+void listFilesInDir(const string& targetPath, const string& fileNameString,
+                    const string& endsWithString) {
 
     DIR* dirFileHandle = opendir(targetPath.c_str());
     if (!dirFileHandle) {
-        // cout << "listFilesInDir() targetPath: " << targetPath <<
-        //           " not read due to error: " << errno << endl;
         return;
     }
 
     struct dirent* dirFileEntry;
     while ((dirFileEntry = readdir(dirFileHandle)) != NULL) {
-        STRING newTargetPath = dirFileEntry->d_name;
-        // cout << "listFilesInDir() reads : " << newTargetPath << " size: " <<
-        //     newTargetPath.length() << endl;
+        string newTargetPath = dirFileEntry->d_name;
 
         // Ignore "movement" entries.
         if (endsWith(newTargetPath, "..") || endsWith(newTargetPath, ".")) {
@@ -99,7 +92,7 @@ void listFilesInDir(const STRING& targetPath, const STRING& fileNameString,
             continue;
         }
 
-        // Process first entry.
+        // Process entry.
         filterAndPrint(newTargetPath, fileNameString, endsWithString);
 
         // If dir/folder, drop in and process all, else we're just done.
@@ -117,12 +110,12 @@ void listFilesInDir(const STRING& targetPath, const STRING& fileNameString,
 // *** Helper method for each dir entry                          ***
 // *****************************************************************
 
-void filterAndPrint(const STRING& targetPath, const STRING& fileNameString,
-                    const STRING& endsWithString) {
+void filterAndPrint(const string& targetPath, const string& fileNameString,
+                    const string& endsWithString) {
 
     // Ensure fileNameString match.
     if (!fileNameString.empty()) {
-        if (targetPath.find(fileNameString) == STRING::npos) {
+        if (targetPath.find(fileNameString) == string::npos) {
             return;
         }
     }
@@ -135,9 +128,42 @@ void filterAndPrint(const STRING& targetPath, const STRING& fileNameString,
     }
 
     // Default, all matchs, so print!
-    // cout << targetPath << endl;
-    STRING systemString = "ls -alFd " + targetPath;
-    system(systemString.c_str());
+    // Shell and query ls ... , collect stdout result string.
+    string resultString;
+
+    const string SYSTEM_STRING = "ls -alFd \"" + targetPath + "\"";
+    if (FILE* stdoutPipe = popen(SYSTEM_STRING.c_str(), "r")) {
+        const int STDOUT_STRING_MAX = 8192;
+
+        array<char, STDOUT_STRING_MAX> buffStrings;
+        while (fgets(buffStrings.data(), STDOUT_STRING_MAX, stdoutPipe) != NULL) {
+            resultString += buffStrings.data();
+        }
+        pclose(stdoutPipe);
+    }
+
+    // Find all column seperators (' ') in ls result String to
+    // enhance the output. File size field needs right align or output
+    // is annoyingly ragged.
+    const int RESULT_COLS_MAX = 5;
+    int resultColIndex[RESULT_COLS_MAX];
+
+    resultColIndex[0] = resultString.find(" ");
+    for (int i = 1; i < RESULT_COLS_MAX; i++) {
+        resultColIndex[i] = resultString.find(" ", resultColIndex[i - 1] + 1);
+    }
+
+    // Right alight the file size column.
+    const int GROUP_COLUMN_INDEX = 3;
+
+    const int SIZE_COLUMN_INDEX = 4;
+    const int SIZE_COLUMN_PAD_TO = 8;
+
+    resultString.insert(resultColIndex[GROUP_COLUMN_INDEX], SIZE_COLUMN_PAD_TO -
+        (resultColIndex[SIZE_COLUMN_INDEX] - resultColIndex[GROUP_COLUMN_INDEX]) + 1, ' ');
+
+    // Endl provided in stdout results.
+    cout << resultString;
 }
 #pragma GCC diagnostic pop
 
@@ -146,12 +172,12 @@ void filterAndPrint(const STRING& targetPath, const STRING& fileNameString,
 // *****************************************************************
 
 static bool
-startsWith(const STRING& str, const STRING& prefix) {
+startsWith(const string& str, const string& prefix) {
     if (prefix.size() > str.size()) {
         return false;
     }
 
-    return std::equal(str.begin(), str.begin() + prefix.size(), prefix.begin());
+    return equal(str.begin(), str.begin() + prefix.size(), prefix.begin());
 }
 
 // *****************************************************************
@@ -159,10 +185,10 @@ startsWith(const STRING& str, const STRING& prefix) {
 // *****************************************************************
 
 static bool
-endsWith(const STRING& str, const STRING& postfix) {
+endsWith(const string& str, const string& postfix) {
     if (postfix.size() > str.size()) {
         return false;
     }
 
-    return std::equal(str.begin() + str.size() - postfix.size(), str.end(), postfix.begin());
+    return equal(str.begin() + str.size() - postfix.size(), str.end(), postfix.begin());
 }
